71cfa3161bf87070f8d5006d012a467fa595ef03
Hongyu Shi <shihongyu1996@gmail.com>
2022-11-01 15:25:15 -0400

    update on 11/01/2022

--- src/CategoricalColor.js
+++ src/CategoricalColor.js
@@ -44,3 +44,12 @@ export function addColors(color, data) {
 
     return colorFunc
 }
+
+export function convertHexToRgba(hex, alpha=1){
+    let r = parseInt(hex.slice(1, 3), 16)
+    let g = parseInt(hex.slice(3, 5), 16)
+    let b = parseInt(hex.slice(5, 7), 16)
+
+    return 'rgba('+r+', ' + g + ', ' + b + ', ' + alpha + ')'
+}
+
